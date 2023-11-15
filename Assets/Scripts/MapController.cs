06f20c2bb1ee1731369cd48326d0f783fe6222a8
Mari <calamarigold@gmail.com>
2023-11-14 19:13:27 -0500

    Optimize mapcontroller, add chunk deactivating

--- Assets/Scripts/MapController.cs
+++ Assets/Scripts/MapController.cs
@@ -1,35 +1,42 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using System.Collections.Generic;
 
 // This class manages the dynamic loading of tilemap chunks in a top-down world
 // as the player character moves through the environment.
 public class MapController : MonoBehaviour
 {
-    public Tilemap tilemap; // Reference to the Tilemap where tiles will be placed
-    public TileBase[] tileChunks; // Array of tile chunks to place on the tilemap
+    public Tilemap tilemap;
+    public TileBase[] tileChunks;
     public int chunkSize = 10;
     public PlayerController playerController;
+    public Camera mainCamera;
 
     private Vector3Int previousChunkPosition;
+    private HashSet<Vector3Int> activeChunks;
+    private float updateThresholdDistance = 10f; // Update chunks when the player moves this distance
+
 
     void Start()
     {
-        PopulateInitialChunks();
+        activeChunks = new HashSet<Vector3Int>();
+        UpdateChunks(true);
     }
-
     void Update()
     {
-        Vector3Int currentChunkPosition = GetCurrentChunkPosition();
-
-        // If the player has moved to a new chunk, update the adjacent chunks
-        if (!currentChunkPosition.Equals(previousChunkPosition))
-        {
-            PopulateAdjacentChunks(currentChunkPosition);
-            previousChunkPosition = currentChunkPosition;
-        }
+        UpdateChunks(false);
     }
 
-    // Calculates the current chunk position based on the player's position
+
+    Rect GetCameraBounds()
+    {
+        float verticalExtend = mainCamera.orthographicSize;
+        float horizontalExtend = verticalExtend * Screen.width / Screen.height; // Aspect ratio adjustment
+        Vector2 cameraPosition = (Vector2)mainCamera.transform.position;
+        return new Rect(cameraPosition.x - horizontalExtend, cameraPosition.y - verticalExtend, horizontalExtend * 2, verticalExtend * 2);
+    }
+
+    // Calculate the current chunk position based on player position
     Vector3Int GetCurrentChunkPosition()
     {
         Vector3 playerPosition = playerController.transform.position;
@@ -39,32 +46,78 @@ public class MapController : MonoBehaviour
             0);
     }
 
-    // Populates the initial nine chunks around the player (center chunk and the eight surrounding ones)
-    void PopulateInitialChunks()
+    // Update the active chunks based on player position
+    void UpdateChunks(bool forceUpdate)
     {
-        Vector3Int initialChunkPosition = GetCurrentChunkPosition();
-        for (int x = -2; x <= 2; x++)
+        // Calculate the distance the player has moved since the last update
+        float distanceMoved = Vector3.Distance(playerController.transform.position, previousChunkPosition);
+
+        if (forceUpdate || distanceMoved > updateThresholdDistance)
         {
-            for (int y = -1; y <= 1; y++)
-            {
-                Vector3Int chunkPosition = new Vector3Int(initialChunkPosition.x + x, initialChunkPosition.y + y, 0);
-                PopulateChunk(chunkPosition);
-            }
+            Rect cameraBounds = GetCameraBounds();
+            cameraBounds.xMin -= chunkSize / 2;
+            cameraBounds.xMax += chunkSize / 2;
+            cameraBounds.yMin -= chunkSize / 2;
+            cameraBounds.yMax += chunkSize / 2;
+
+            UpdateChunkStatus(cameraBounds);
+            previousChunkPosition = GetCurrentChunkPosition();
         }
-        previousChunkPosition = initialChunkPosition;
     }
 
-    // Populates the chunks adjacent to the current chunk the player is in
-    void PopulateAdjacentChunks(Vector3Int currentChunkPosition)
+
+
+    void UpdateChunkStatus(Rect cameraBounds)
     {
-        for (int x = -2; x <= 2; x++)
+        int viewDistanceInChunks = 5;
+        Vector2 cameraVelocity = mainCamera.GetComponent<Rigidbody2D>()?.velocity ?? Vector2.zero;
+        int predictiveLoadingFactor = Mathf.CeilToInt(cameraVelocity.magnitude / chunkSize);
+
+        // Store the current chunk position at the start
+        Vector3Int currentChunkPosition = GetCurrentChunkPosition();
+
+        HashSet<Vector3Int> updatedActiveChunks = new HashSet<Vector3Int>();
+
+        for (int x = -viewDistanceInChunks - predictiveLoadingFactor; x <= viewDistanceInChunks + predictiveLoadingFactor; x++)
         {
-            for (int y = -2; y <= 2; y++)
+            for (int y = -viewDistanceInChunks - predictiveLoadingFactor; y <= viewDistanceInChunks + predictiveLoadingFactor; y++)
             {
-                Vector3Int chunkPosition = new Vector3Int(currentChunkPosition.x + x, currentChunkPosition.y + y, 0);
-                PopulateChunk(chunkPosition);
+                Vector3Int chunkPosition = currentChunkPosition + new Vector3Int(x, y, 0);
+                if (IsChunkInCameraBounds(chunkPosition, cameraBounds))
+                {
+                    if (!activeChunks.Contains(chunkPosition))
+                    {
+                        PopulateChunk(chunkPosition);
+                    }
+                    updatedActiveChunks.Add(chunkPosition);
+                }
+                else if (activeChunks.Contains(chunkPosition))
+                {
+                    DeactivateChunk(chunkPosition);
+                }
             }
         }
+
+        activeChunks = updatedActiveChunks;
+    }
+
+
+    
+
+    bool IsChunkInCameraBounds(Vector3Int chunkPosition, Rect cameraBounds)
+    {
+        // Calculate the bounds of the chunk considering its full size.
+        Vector2 chunkCenter = new Vector2(chunkPosition.x * chunkSize + chunkSize / 2, chunkPosition.y * chunkSize + chunkSize / 2);
+        Rect chunkRect = new Rect(chunkCenter.x - chunkSize / 2, chunkCenter.y - chunkSize / 2, chunkSize, chunkSize);
+
+        // Inflate the camera bounds slightly to ensure chunks at the edge are included.
+        Rect inflatedCameraBounds = cameraBounds;
+        inflatedCameraBounds.xMin -= chunkSize;
+        inflatedCameraBounds.xMax += chunkSize;
+        inflatedCameraBounds.yMin -= chunkSize;
+        inflatedCameraBounds.yMax += chunkSize;
+
+        return inflatedCameraBounds.Overlaps(chunkRect);
     }
 
     // Populates a single chunk with tiles at the specified chunkPosition
@@ -86,4 +139,18 @@ public class MapController : MonoBehaviour
             }
         }
     }
+
+
+    void DeactivateChunk(Vector3Int chunkPosition)
+    {
+        for (int x = 0; x < chunkSize; x++)
+        {
+            for (int y = 0; y < chunkSize; y++)
+            {
+                Vector3Int tilePosition = chunkPosition * chunkSize + new Vector3Int(x, y, 0);
+                tilemap.SetTile(tilePosition, null);
+            }
+        }
+    }
+
 }
